b820d677daf3bdddbb56348eafb88a94ae4d191b
sedin <sedin@kakao.com>
2021-03-22 15:01:47 +0900

    init: spring 프로젝트 초기화
    - 프로젝트 생성

---
+++ src/main/java/com/first/spring/firstspring/FirstSpringApplication.java
@@ -0,0 +1,13 @@
+package com.first.spring.firstspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringApplication.class, args);
+    }
+
+}
